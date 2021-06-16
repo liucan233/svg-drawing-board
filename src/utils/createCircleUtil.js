9f708f940d52e59b868fd96dd229eff3f40722b0
iCyre <313720186@qq.com>
2021-06-16 16:49:03 +0800

    fix: 绘制圆形鼠标移动圆形增大过快

--- src/utils/createCircleUtil.js
+++ src/utils/createCircleUtil.js
@@ -1,19 +1,24 @@
 function createCircle(e, figures) {
   const { nativeEvent, type } = e;
   const circle = figures.pop();
-  const dx = nativeEvent.offsetX - circle.downX;
-  const dy = nativeEvent.offsetY - circle.downY;
-  circle.r = ~~Math.sqrt(dx * dx + dy * dy);
+  const { downX, downY } = circle;
+  circle.cx = (downX + nativeEvent.offsetX) / 2;
+  circle.cy = (downY + nativeEvent.offsetY) / 2;
+  circle.r =
+    Math.max(
+      Math.abs(nativeEvent.offsetX - downX),
+      Math.abs(nativeEvent.offsetY - downY)
+    ) / 2;
   figures.push(circle);
 }
 
 function renderCircle(figure) {
-  const { downX, downY, r, key, color, fill, width } = figure;
+  const { cx, cy, r, key, color, fill, width } = figure;
   return (
     <circle
       r={r}
-      cx={downX}
-      cy={downY}
+      cx={cx}
+      cy={cy}
       key={key}
       stroke={color}
       fill="transparent"
